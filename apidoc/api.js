72ab0c5f67f79aea1d28b3f682fee159873733ac
Stefan Schneider <eqsoftdev@gmail.com>
2013-06-16 20:02:50 +0200

    adding apidoc generated with YUIDoc

---
+++ apidoc/api.js
@@ -0,0 +1,14 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "db",
+        "env",
+        "gui",
+        "main",
+        "som",
+        "utils"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
