c216018db7b1a6b3ed996308712833904b5669a4
Stefan Schneider <eqsoftdev@gmail.com>
2013-07-07 22:59:49 +0200

    edit sopConnector

--- ilias/sopConnector.php
+++ ilias/sopConnector.php
@@ -12,10 +12,14 @@ echo "<!DOCTYPE html>
 	</head>
 	<body>
 		<h3>CORS Test</h3>
-		<div><input type=\"button\" value=\"open client0_205\" onclick=\"openLm('client0_205')\"/></div><br />
-		<div><input type=\"button\" value=\"open client0_206\" onclick=\"openLm('client0_206')\"/></div><br />
-		<div><input type=\"button\" value=\"import client0_205\" onclick=\"importLm('client0_205')\"/></div><br />
-		<div><input type=\"button\" value=\"import client0_206\" onclick=\"importLm('client0_206')\"/></div><br />
+		<div id=\"divCheckSopConnector\" style=\"display:block\">checking for sopConnector</div>
+		<div id=\"divOfflineManager\" style=\"display:none\">
+			<div><input type=\"button\" value=\"open client0_205\" onclick=\"openLm('client0_205')\"/></div><br />
+			<div><input type=\"button\" value=\"open client0_206\" onclick=\"openLm('client0_206')\"/></div><br />
+			<div><input type=\"button\" value=\"import client0_207\" onclick=\"importLm('client0_207')\"/></div><br />
+			<div><input type=\"button\" value=\"import client0_208\" onclick=\"importLm('client0_208')\"/></div><br />
+			<div><input type=\"button\" value=\"import tracking\" onclick=\"importTracking('client0_208','55')\"/></div><br />
+		</div>
 		<div id=\"out\"></div>
 	</body>
 </html>"; 
